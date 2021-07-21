df4553b94c0c8e9afd8c67c9f5cbef34b78f2d2a
Scott Stevens <scott.axios@gmail.com>
2021-07-21 14:58:04 -0500

    first major commit, base code upload

--- bizzystats/frmMain.Designer.vb
+++ bizzystats/frmMain.Designer.vb
@@ -22,10 +22,154 @@ Partial Class frmMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.fd1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.txtLogFolder = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cmbFilter = New System.Windows.Forms.ComboBox()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.txtOutput = New System.Windows.Forms.RichTextBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.sd1 = New System.Windows.Forms.SaveFileDialog()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.status = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(271, 8)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(73, 22)
+        Me.btnBrowse.TabIndex = 0
+        Me.btnBrowse.Text = "Browse"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'txtLogFolder
+        '
+        Me.txtLogFolder.Location = New System.Drawing.Point(78, 9)
+        Me.txtLogFolder.Name = "txtLogFolder"
+        Me.txtLogFolder.Size = New System.Drawing.Size(187, 20)
+        Me.txtLogFolder.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Log Folder:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Search Filter:"
+        '
+        'cmbFilter
+        '
+        Me.cmbFilter.FormattingEnabled = True
+        Me.cmbFilter.Items.AddRange(New Object() {"say", "say_team"})
+        Me.cmbFilter.Location = New System.Drawing.Point(78, 32)
+        Me.cmbFilter.Name = "cmbFilter"
+        Me.cmbFilter.Size = New System.Drawing.Size(187, 21)
+        Me.cmbFilter.TabIndex = 4
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Enabled = False
+        Me.btnSearch.Location = New System.Drawing.Point(271, 32)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(73, 22)
+        Me.btnSearch.TabIndex = 5
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(15, 60)
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.Size = New System.Drawing.Size(403, 336)
+        Me.txtOutput.TabIndex = 6
+        Me.txtOutput.Text = ""
+        Me.txtOutput.WordWrap = False
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(344, 32)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(73, 22)
+        Me.btnSave.TabIndex = 7
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Location = New System.Drawing.Point(345, 8)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(73, 22)
+        Me.btnLoad.TabIndex = 8
+        Me.btnLoad.Text = "Load"
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.status})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 399)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(430, 22)
+        Me.StatusStrip1.TabIndex = 9
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'status
+        '
+        Me.status.Name = "status"
+        Me.status.Size = New System.Drawing.Size(0, 17)
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(430, 421)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.cmbFilter)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtLogFolder)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "bizzystats"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents fd1 As FolderBrowserDialog
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents txtLogFolder As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cmbFilter As ComboBox
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents sd1 As SaveFileDialog
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents status As ToolStripStatusLabel
+    Public WithEvents txtOutput As RichTextBox
 End Class
--- bizzystats/frmMain.vb
+++ bizzystats/frmMain.vb
@@ -1,3 +1,120 @@
-﻿Public Class frmMain
+﻿Option Explicit On
 
+Imports System.IO
+'Imports Excel = Microsoft.Office.Interop.Excel
+
+Public Class frmMain
+    Dim currentLogsFound As New ArrayList
+    Dim currentStatsFound As New ArrayList
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmbFilter.SelectedIndex = 1
+    End Sub
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        fd1.SelectedPath = ""
+        fd1.ShowDialog()
+
+        txtLogFolder.Text = fd1.SelectedPath
+    End Sub
+
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+        If txtLogFolder.Text = "" Then
+            btnSearch.Enabled = False
+            Exit Sub
+        End If
+
+        currentLogsFound.Clear()
+        currentLogsFound = FindLogs(txtLogFolder.Text)
+        status.Text = "Loaded " & currentLogsFound.Count & " log files"
+
+        If currentLogsFound.Count > 0 Then
+            btnSearch.Enabled = True
+        Else
+            btnSearch.Enabled = False
+        End If
+
+    End Sub
+
+    Private Function FindLogs(folderName As String) As ArrayList
+
+        Dim tempLogFileList As New ArrayList
+
+        If folderName = "" Then Return tempLogFileList
+
+        Dim diLogs As New DirectoryInfo(folderName)
+        Dim fiLogs As FileInfo() = diLogs.GetFiles()
+
+        Dim friLog As FileInfo
+        For Each friLog In fiLogs
+            tempLogFileList.Add(friLog.FullName)
+        Next friLog
+
+        Return tempLogFileList
+    End Function
+
+    Private Function FindStats2(WhatToFind As String, filename As String) As ArrayList
+
+        Dim tempStats As New ArrayList
+
+        For Each Line As String In File.ReadLines(filename)
+            If Line.Contains(WhatToFind) = True Then
+                tempStats.Add(Line)
+            End If
+        Next
+
+        Return tempStats
+    End Function
+
+    Private Function FindStats(WhatToFind As String, filename As String) As ArrayList
+
+        Dim tempStats As New ArrayList
+
+        For Each Line As String In File.ReadLines(filename)
+            If Line.Contains(WhatToFind) = True Then
+                tempStats.Add(Line)
+            End If
+        Next
+
+        Return tempStats
+    End Function
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        If txtOutput.Text = "" Then Exit Sub
+
+        sd1.FileName = ""
+        sd1.ShowDialog()
+
+        If sd1.FileName = "" Then Exit Sub
+
+        txtOutput.SaveFile(sd1.FileName)
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        If currentLogsFound.Count < 1 Then Exit Sub
+        If cmbFilter.Text = "" Then Exit Sub
+
+        txtOutput.Clear()
+        currentStatsFound.Clear()
+        Dim tempStats As New ArrayList
+
+        For Each logFile As String In currentLogsFound
+            'get all the found lines
+            tempStats = FindStats(cmbFilter.Text, logFile)
+
+            'clean the output here
+
+            For Each tempStat As String In tempStats
+                currentStatsFound.Add(tempStat)
+                txtOutput.AppendText(tempStat & vbNewLine)
+            Next
+        Next
+
+    End Sub
+
+    Private Sub frmMain_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        On Error Resume Next
+        txtOutput.Width = Me.Width - 43
+        txtOutput.Height = Me.Height - 124
+    End Sub
 End Class
